ed79da42e5971fcb9590079e6c2c71930cf10983
Nkot117 <kumakura117@gmail.com>
2024-06-10 09:54:29 +0900

    コルーチンライブラリを依存関係に追加

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 package org.example
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 fun main() {
-    val name = "Kotlin"
-    //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-    // IntelliJ IDEA によるその修正案を確認できます。
-    println("Hello, " + name + "!")
+    runBlocking {
+        launch {
+            println("1")
+            delay(1000L)
+            println("2")
+        }
 
-    for (i in 1..5) {
-        //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-        // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-        println("i = $i")
+        launch {
+            println("3")
+        }
     }
+
 }
